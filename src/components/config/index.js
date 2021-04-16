fd4c2d1c6cfdf336ea585902fc969ba0582ec2b6
Beagbarreto <beagbarreto09@gmail.com>
2021-04-15 20:41:37 -0500

    feat: updated Api key due to exeeded quota

--- src/components/config/index.js
+++ src/components/config/index.js
@@ -1,8 +1,7 @@
 const config = {
-    api_key: 'AIzaSyCTk2umL2c3qd1A9xY_0NKkAj0aZ7C2Pfo',
+    api_key: 'AIzaSyBaJ6Hbyo4fzS6JZLNN5QRfJEZQLyIPuz4',
     wizeline_channel_id: 'UCPGzT4wecuWM0BH9mPiulXg',
     wizeline_uploads: 'UUPGzT4wecuWM0BH9mPiulXg',
-    wow_channel_id: 'UC0sEIyXXalzD1lbwm3D2xpA'
 }
 
 export default config;

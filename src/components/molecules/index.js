cabadc6dd98da5eadc63c72fc7884856775780de
Beagbarreto <beagbarreto09@gmail.com>
2021-04-28 08:53:46 -0500

    refactor: removed unused components

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,6 +1,5 @@
-import LongVideoCard from './LongVideoCard/LongVideoCard.molecule';
 import SearchBar from './SearchBar/SearchBar.molecule';
 import SideBar from './SideBar/SideBar.molecule';
 import VideoCard from './VideoCard/VideoCard.molecule';
 
-export { SearchBar, SideBar, LongVideoCard, VideoCard };
+export { SearchBar, SideBar, VideoCard };
